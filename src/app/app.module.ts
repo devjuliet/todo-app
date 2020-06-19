6858944cc96f869779648c7c5fe959e634ee97d0
Julieta <juliiy98@gmail.com>
2020-06-18 21:25:32 -0600

    added classes components and service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodoItemComponent } from './components/todo-item/todo-item.component';
+import { TodoContainerComponent } from './components/todo-container/todo-container.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoItemComponent,
+    TodoContainerComponent
   ],
   imports: [
     BrowserModule,
