8f75843246799930b4d7169c313c0c441d1b9aa4
Julieta <juliiy98@gmail.com>
2020-07-01 22:40:59 -0600

    services functions added

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { User } from '../models/user';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
@@ -10,7 +11,7 @@ export class AuthenticationService {
   private currentUserSubject: BehaviorSubject<User>;
   public currentUser: Observable<User>;
 
-  constructor(http: HttpClient) {
+  constructor(private http: HttpClient) {
     this.currentUserSubject = new BehaviorSubject<User>(
       JSON.parse(localStorage.getItem('currentUser'))
     );
@@ -23,9 +24,13 @@ export class AuthenticationService {
 
   login(username, password) {
     return this.http
-      .post<any>(`${config.apiUrl}/users/authenticate`, { username, password })
+      .post<any>(`http://localhost:4200/users/authenticate`, {
+        username,
+        password,
+      })
       .pipe(
         map((user) => {
+          // store user details and jwt token in local storage to keep user logged in between page refreshes
           localStorage.setItem('currentUser', JSON.stringify(user));
           this.currentUserSubject.next(user);
           return user;
